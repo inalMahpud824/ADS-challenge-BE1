48e429ee85b1c5398ba4377380afd6a02f207c49
Inal Mahpud <inalmahpud1@gmail.com>
2024-01-03 15:38:54 +0700

    feat: use routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const port = require('./config/index')
+const {port} = require('./config/index')
 const app = express()
+const routes = require('./routes')
+app.use(express.json())
+
+app.use(routes)
 app.listen(port, () => {
   console.log('server berjalan ' + port)
 })
